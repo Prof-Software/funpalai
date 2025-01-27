0d159b28f5ba90af61302cf2fa6f5cc8c7d5ce5d
Optimistique3452 <kartikeyasharma2006@gmail.com>
2025-01-27 23:58:00 +0530

    made firebase public for now

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup,signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
+  apiKey: "AIzaSyCGftNdJfZOmi3zzKQ9pPYHueYGO6cgyWc",
   authDomain: "funpalai.firebaseapp.com",
   projectId: "funpalai",
   storageBucket: "funpalai.firebasestorage.app",
